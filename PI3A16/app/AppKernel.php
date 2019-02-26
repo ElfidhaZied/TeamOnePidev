c95ec530e745e5a1f589621f39a66c26449b3896
ElfidhaZied <47177324+ElfidhaZied@users.noreply.github.com>
2019-02-27 00:08:59 +0100

    zied

--- PI3A16/app/AppKernel.php
+++ PI3A16/app/AppKernel.php
@@ -18,8 +18,15 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new CMEN\GoogleChartsBundle\CMENGoogleChartsBundle(),
             new EventBundle\EventBundle(),
             new UserBundle\UserBundle(),
+            new CommunicationBundle\CommunicationBundle(),
+            new ReclamationBundle\ReclamationBundle(),
+            new ProfilingBundle\ProfilingBundle(),
+            new ReservationBundle\ReservationBundle(),
+            new BlogBundle\BlogBundle(),
+            new BackOfficeIndexBundle\BackOfficeIndexBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
